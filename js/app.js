6b757ac4bac780ae489e08179c269326a6364303
AndreaGiacometti <159144064+AndreaGiacometti@users.noreply.github.com>
2024-03-20 17:57:26 +0100

    ADD index htm and comments for the first exercise

---
+++ js/app.js
@@ -0,0 +1,7 @@
+console.log('Paliedispari');
+
+// Palidroma
+// - chiedere all’utente di inserire una parola 
+// Creare una funzione per capire se la parola inserita è palindroma. 
+// Questa funzione deve ricevere come input la parola inserita dall’utente e 
+// ritornare true se la parola è palindroma e false se la parola non è palindroma
